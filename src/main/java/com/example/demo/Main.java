b9ef08677ebdebd90e8d5033a188499f8cceb83c
TheMuntan <serdar_tst@hotmail.com>
2021-04-22 08:48:11 +0200

    rename to main

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class Main {
 	public static void main(String[] args) {
-	SpringApplication.run(DemoApplication.class, args);
+	SpringApplication.run(Main.class, args);
 	}
 	
 	@GetMapping("/hello")
